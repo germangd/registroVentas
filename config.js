3dd1525bd343dfca0873a1b3f39308d47b57e514
germangd <63747983+germangd@users.noreply.github.com>
2026-04-12 14:56:31 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 //  COMPLETÁ ESTOS 3 VALORES (instrucciones en LEEME.txt)
 // ─────────────────────────────────────────────────────────
 window.APP_CONFIG = {
-  SHEET_ID:   '1pAjybwB2u8uV49vqoaDqiQmp5oOlF_yI',    // ID de tu Google Sheet
+  SHEET_ID:   '1Qbj03cGXZrDFdDy4Ol7NGRivPfJ1b9-6dRyX0Isv-v0',    // ID de tu Google Sheet
   API_KEY:    'AIzaSyAGb1yrGmQQ5Yx60q55V41lavM0gHirRAc',     // API Key de Google
   SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzVGaH6zCEiNwqOy9NM3jP5f6sTbkg4BKz4hb0zz4MtC7HPF8Vjdm32pZZuHTKsAswz/exec', // URL del Apps Script
 };
