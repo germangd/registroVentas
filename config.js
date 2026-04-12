2ac7eebab460a171bf0122c4ed75c64189e53471
germangd <63747983+germangd@users.noreply.github.com>
2026-04-12 15:22:17 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 window.APP_CONFIG = {
   SHEET_ID:   '1Qbj03cGXZrDFdDy4Ol7NGRivPfJ1b9-6dRyX0Isv-v0',    // ID de tu Google Sheet
   API_KEY:    'AIzaSyAGb1yrGmQQ5Yx60q55V41lavM0gHirRAc',     // API Key de Google
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzVGaH6zCEiNwqOy9NM3jP5f6sTbkg4BKz4hb0zz4MtC7HPF8Vjdm32pZZuHTKsAswz/exec', // URL del Apps Script
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwUXUyP214mSelsBktvMIhcfoZwHKdLrDylq1I1_UvC2B-BwO8wXr6mCi9-pFdAqL2wuw/exec', // URL del Apps Script
 };
