dc810b7c6081f0202cd4c952078a2d2234f3d3a0
germangd <63747983+germangd@users.noreply.github.com>
2026-04-20 00:22:58 -0300

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 window.APP_CONFIG = {
   SHEET_ID:   '1Qbj03cGXZrDFdDy4Ol7NGRivPfJ1b9-6dRyX0Isv-v0',    // ID de tu Google Sheet
   API_KEY:    'AIzaSyAGb1yrGmQQ5Yx60q55V41lavM0gHirRAc',     // API Key de Google
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzp-afPdmbRCbQ9Y5AYzkO953UKecQwn_OYLixV4UTpyz9Hhb3Dn92Khquq-dtDNX5LBg/exec', // URL del Apps Script
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyRt_LYsvjCQ2mClfwZWxSsLz_hakTNEdEGG0xo6WX71DqjZ4lMGzHeMEP0sidyNyUpng/exec', // URL del Apps Script
 };
